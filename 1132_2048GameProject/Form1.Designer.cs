714e13d5fc3f5bf0884d0273d757e4d4f93bba8b
ChengZeLiu4322 <s11313672@o365.pu.edu.tw>
2025-05-18 19:41:01 +0800

    加入專案檔案。

---
+++ 1132_2048GameProject/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace _1132_2048GameProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(426, 426);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "2048";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ 1132_2048GameProject/Form1.cs
@@ -0,0 +1,213 @@
+namespace _1132_2048GameProject
+{
+    public partial class Form1 : Form
+    {
+        private int[,] board = new int[4, 4];
+        private Label[,] labels = new Label[4, 4];
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBoard();
+            AddRandomTile();
+            AddRandomTile();
+            UpdateUI();
+            this.KeyDown += MainForm_KeyDown;
+            this.KeyPreview = true;
+
+        }
+        private void InitializeBoard()
+        {
+            // 將 labels[,] 對應到 TableLayoutPanel 中的 Label 控制項
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.ColumnCount = 4;
+
+            // 設定 TableLayoutPanel 的行和列的大小
+            for (int i = 0; i < 4; i++)
+            {
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25));
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+            }
+
+            // 設定 Label 控制項的屬性
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    Label lbl = new Label();
+                    lbl.Dock = DockStyle.Fill;
+                    lbl.TextAlign = ContentAlignment.MiddleCenter; // 設定文字置中
+                    lbl.Font = new Font("Microsoft JhengHei", 12, FontStyle.Bold);
+                    lbl.BackColor = Color.LightGray;
+                    lbl.Margin = new Padding(5);
+                    tableLayoutPanel1.Controls.Add(lbl, col, row);
+                    labels[row, col] = lbl;
+                }
+            }
+        }
+        private void AddRandomTile()
+        {
+            // 找到空格子隨機放 2 或 4
+            var empty = new System.Collections.Generic.List<(int, int)>();
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (board[i, j] == 0)
+                        empty.Add((i, j));
+
+            if (empty.Count == 0) return;
+
+            var (x, y) = empty[rand.Next(empty.Count)];
+            board[x, y] = rand.Next(10) == 0 ? 4 : 2;
+        }
+        private void UpdateUI()
+        {
+            // 把 board 裡的數字更新到 Label
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int val = board[i, j];
+                    labels[i, j].Text = val == 0 ? "" : val.ToString();
+                    labels[i, j].BackColor = GetTileColor(val);
+                    labels[i, j].ForeColor = val <= 4 ? Color.Black : Color.White;
+                }
+            }
+        }
+        //上色
+        private Color GetTileColor(int value)
+        {
+            return value switch
+            {
+                0 => Color.LightGray,
+                2 => Color.Beige,
+                4 => Color.BurlyWood,
+                8 => Color.Orange,
+                16 => Color.DarkOrange,
+                32 => Color.OrangeRed,
+                64 => Color.Red,
+                128 => Color.YellowGreen,
+                256 => Color.Green,
+                512 => Color.Teal,
+                1024 => Color.MediumBlue,
+                2048 => Color.Gold,
+                _ => Color.Black
+            };
+        }
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 判斷按鍵並移動數字
+            bool moved = false;
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    moved = MoveLeft();
+                    break;
+                case Keys.Right:
+                    moved = MoveRight();
+                    break;
+                case Keys.Up:
+                    moved = MoveUp();
+                    break;
+                case Keys.Down:
+                    moved = MoveDown();
+                    break;
+            }
+
+            if (moved)
+            {
+                AddRandomTile();
+                UpdateUI();
+                if (CheckGameOver())
+                    MessageBox.Show("遊戲結束！");
+            }
+        }
+        // 移動數字
+        private bool MoveLeft()
+        {
+            bool moved = false;// 是否移動過
+            // 合併數字
+            for (int i = 0; i < 4; i++)
+            {
+                int[] row = new int[4];
+                int index = 0;
+                bool merged = false;
+                // 將行的數字移到左邊
+                for (int j = 0; j < 4; j++)
+                {
+                    if (board[i, j] != 0)
+                    {
+                        if (index > 0 && row[index - 1] == board[i, j] && !merged)
+                        {
+                            row[index - 1] *= 2;
+                            merged = true;
+                            moved = true;
+                        }
+                        else
+                        {
+                            row[index++] = board[i, j];
+                            if (j != index - 1) moved = true;
+                            merged = false;
+                        }
+                    }
+                }
+
+                for (int j = 0; j < 4; j++)
+                    board[i, j] = row[j];
+            }
+            return moved;
+        }
+        private bool MoveRight()
+        {
+            ReverseRows();
+            bool moved = MoveLeft();
+            ReverseRows();
+            return moved;
+        }
+        private bool MoveUp()
+        {
+            Transpose();
+            bool moved = MoveLeft();
+            Transpose();
+            return moved;
+        }
+        private bool MoveDown()
+        {
+            Transpose();
+            ReverseRows();
+            bool moved = MoveLeft();
+            ReverseRows();
+            Transpose();
+            return moved;
+        }
+        // 反轉行
+        private void ReverseRows()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    (board[i, j], board[i, 3 - j]) = (board[i, 3 - j], board[i, j]);
+                }
+            }
+        }
+        // 轉置矩陣
+        private void Transpose()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = i + 1; j < 4; j++)
+                    (board[i, j], board[j, i]) = (board[j, i], board[i, j]);
+        }
+
+        private bool CheckGameOver()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (board[i, j] == 0 || // 判斷是否有空格
+                        (j < 3 && board[i, j] == board[i, j + 1]) || // 還可水平合併
+                        (i < 3 && board[i, j] == board[i + 1, j])) // 還可垂直合併
+                        return false;
+            return true;
+        }
+
+    }
+}
